afc2dc0f1cabd47c923446d90afe259eaa1bc7dc
roman.grishin.me@gmail.com <roman.grishin.me@gmail.com>
2023-04-07 09:56:54 +0600

    feature: sidenav links follow to section

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import './styles/main.scss';
 import { toggleMenu } from './script/sideNav';
 import { addHoverToListElement } from './script/cardHover';
 
-window.addEventListener('scroll', toggleMenu);
-
 const protfolioCards = document.querySelectorAll('.portfolio__cards');
 const stackCards = document.querySelectorAll('.stack__list-item')
+
+window.addEventListener('scroll', toggleMenu);
 
 addHoverToListElement(protfolioCards);
 addHoverToListElement(stackCards);
