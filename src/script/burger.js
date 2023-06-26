9b81e50a173718a01626a587b5112496f7af87de
roman.grishin.me@gmail.com <roman.grishin.me@gmail.com>
2023-06-26 20:28:05 +0600

    refactor: mobile navigation removed to sidenav from header

--- src/script/burger.js
+++ src/script/burger.js
@@ -1,15 +1,15 @@
-const burgerButton = document.querySelector('.header__burger');
-const mobileMenu = document.querySelector('.header__menu');
-const mobileWrapper = document.querySelector('.header__wrapper');
+const burgerButton = document.querySelector('.sidenav__burger');
+const mobileMenu = document.querySelector('.sidenav__menu');
+const mobileWrapper = document.querySelector('.sidenav__wrapper');
 
 function openMenu() {
-  mobileMenu.classList.add('header__menu_active');
-  mobileWrapper.classList.add('header__wrapper_active');
+  mobileMenu.classList.add('sidenav__menu_active');
+  mobileWrapper.classList.add('sidenav__wrapper_active');
 }
 
 function closeMenu() {
-  mobileMenu.classList.remove('header__menu_active');
-  mobileWrapper.classList.remove('header__wrapper_active');
+  mobileMenu.classList.remove('sidenav__menu_active');
+  mobileWrapper.classList.remove('sidenav__wrapper_active');
 }
 
 function handleMenuClick() {
@@ -24,7 +24,7 @@ function handleMenuClick() {
 
   mobileMenu.addEventListener('click', (evt) => {
 
-    if(evt.target.classList.contains('header__menu-link')) {
+    if(evt.target.classList.contains('sidenav__menu-link')) {
       closeMenu();
     }
   })
